ccdb4f068b7d855abd2223b19fcbe6f54f5f2015
sams <sams@indeed.com>
2014-10-26 21:13:59 -0500

    First push

---
+++ custom.el
@@ -0,0 +1,108 @@
+;;; This file contains some temporary code snippets, it will be loaded after
+;;; various oh-my-emacs modules. When you just want to test some code snippets
+;;; and don't want to bother with the huge ome.*org files, you can put things
+;;; here.
+
+;; For example, oh-my-emacs disables menu-bar-mode by default. If you want it
+;; back, just put following code here.
+(menu-bar-mode t)
+
+;;; You email address
+(setq user-mail-address "xiaohanyu1988@gmail.com")
+
+;;; Calendar settings
+;; you can use M-x sunrise-sunset to get the sun time
+(setq calendar-latitude 39.9)
+(setq calendar-longitude 116.3)
+(setq calendar-location-name "Beijing, China")
+
+;;; Time related settings
+;; show time in 24hours format
+(setq display-time-24hr-format t)
+;; show time and date
+(setq display-time-and-date t)
+;; time change interval
+(setq display-time-interval 10)
+;; show time
+(display-time-mode t)
+
+;;; Some tiny tool functions
+(defun replace-all-chinese-quote ()
+  (interactive)
+  (save-excursion
+    (mark-whole-buffer)
+    (replace-regexp  "”\\|“" "\"")
+    (mark-whole-buffer)
+    (replace-regexp "’\\|‘" "'")))
+
+;; Comment function for GAS assembly language
+(defun gas-comment-region (start end)
+  "Comment region for AT&T syntax assembly language The default
+comment-char for gas is ';', we need '#' instead"
+  (interactive "r")
+  (setq end (copy-marker end t))
+  (save-excursion
+    (goto-char start)
+    (while (< (point) end)
+      (beginning-of-line)
+      (insert "# ")
+      (next-line))
+    (goto-char end)))
+
+(defun gas-uncomment-region (start end)
+  "Uncomment region for AT&T syntax assembly language the
+inversion of gas-comment-region"
+  (interactive "r")
+  (setq end (copy-marker end t))
+  (save-excursion
+    (goto-char start)
+    (while (< (point) end)
+      (beginning-of-line)
+      (if (equal (char-after) ?#)
+          (delete-char 1))
+      (next-line))
+    (goto-char end)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "943bff6eada8e1796f8192a7124c1129d6ff9fbd1a0aed7b57ad2bf14201fdd4" default)))
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(inhibit-startup-screen t)
+ '(magit-diff-use-overlays nil)
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#839496" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,96 @@
+;;; init.el --- Where all the magic begins
+;;
+;; Part of the oh-my-emacs
+;;
+;; This is the first thing to get loaded.
+;;
+
+;; Enter debugger if an error is signaled during Emacs startup.
+;;
+;; This works the same as you boot emacs with "--debug-init" every time, except
+;; for errors in "init.el" itself, which means, if there's an error in
+;; "init.el", "emacs --debug-init" will entering the debugger, while "emacs"
+;; will not; however, if there's an error in other files loaded by init.el,
+;; both "emacs" and "emacs --debug-init" will entering the debugger. I don't
+;; know why.
+(setq debug-on-error t)
+
+;; Now install el-get at the very first
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch
+          ;; do not build recipes from emacswiki due to poor quality and
+          ;; documentation
+          el-get-install-skip-emacswiki-recipes)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  ;; build melpa packages for el-get
+  (el-get-install 'package)
+  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                           ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (el-get-elpa-build-local-recipes))
+
+;; enable git shallow clone to save time and bandwidth
+(setq el-get-git-shallow-clone t)
+
+;; Sometimes, we need to experiment with our own recipe, or override the
+;; default el-get recipe to get around bugs.
+(add-to-list 'el-get-recipe-path "~/.emacs.d/ome-el-get-recipes")
+
+;; Some workaround for emacs version < 24.0, thanks Silthanis@github.
+(if (< emacs-major-version 24)
+    (defun file-name-base (&optional filename)
+      "Return the base name of the FILENAME: no directory, no extension.
+FILENAME defaults to `buffer-file-name'."
+      (file-name-sans-extension
+       (file-name-nondirectory (or filename (buffer-file-name))))))
+
+;; Oh-my-emacs adopt org-mode 8.x from v0.3, so org-mode should be the first
+;; package to be installed via el-get
+(defun ome-org-mode-setup ()
+  ;; markdown export support
+  (require 'ox-md))
+
+(add-to-list 'el-get-sources
+             '(:name org-mode
+                     :after (progn
+                              (ome-org-mode-setup))))
+
+(el-get 'sync (mapcar 'el-get-source-name el-get-sources))
+
+(defvar ome-dir (file-name-directory (or load-file-name (buffer-file-name)))
+  "oh-my-emacs home directory.")
+
+;; load up the ome
+(org-babel-load-file (expand-file-name "ome.org" ome-dir))
+
+(global-set-key (kbd "M-x") 'execute-extended-command)
+
+(require 'ido)
+(ido-mode t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load "~/.emacs.d/themes/zenburn.el")
+   
+ ;;; C-c as general purpose escape key sequence.
+   ;;;
+(defun my-esc (prompt)
+  "Functionality for escaping generally.  Includes exiting Evil insert state and C-g binding. "
+  (cond
+   ;; If we're in one of the Evil states that defines [escape] key, return [escape] so as
+   ;; Key Lookup will use it.
+   ((or (evil-insert-state-p) (evil-normal-state-p) (evil-replace-state-p) (evil-visual-state-p)) [escape])
+   ;; This is the best way I could infer for now to have C-c work during evil-read-key.
+   ;; Note: As long as I return [escape] in normal-state, I don't need this.
+   ;;((eq overriding-terminal-local-map evil-read-key-map) (keyboard-quit) (kbd ""))
+   (t (kbd "C-g"))))
+(define-key key-translation-map (kbd "C-g") 'my-esc)
+
+
+(load "~/.emacs.d/keychord.el")
+(key-chord-mode 1)
+(key-chord-define evil-insert-state-map ";;" 'evil-normal-state)
+;;; init.el ends here
